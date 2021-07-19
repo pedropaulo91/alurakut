1f618ab56bd5e1fa9ad3b5b949e460fd8567ee9c
Pedro Paulo <pedroo.pauloo@outlook.com>
2021-07-18 22:16:24 -0300

    Reposicionamento dos box da página

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,6 @@ export default function Home(props) {
         return respostaDoServidor.json();
       })
       .then((respostaCompleta) => {
-        console.log(respostaCompleta);
         setSeguidores(respostaCompleta);
       })
 
@@ -140,9 +139,9 @@ export default function Home(props) {
 
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
 
-          <ProfileRelationsBox title="Seguidores" items={seguidores} type="seguidores" />
           <ProfileRelationsBox title="Comunidades" items={comunidades} type="comunidades" />
           <ProfileRelationsBox title="Pessoas da comunidade" items={pessoasFavoritas} type="pessoasDaComunidade" />
+          <ProfileRelationsBox title="Seguidores" items={seguidores} type="seguidores" />
 
         </div>
       </MainGrid>
